f6117a6a23c3a5199d085a9b75f67eda91549978
Charles Wesley Cho <charleswcho@gmail.com>
2021-02-11 20:45:28 -0800

    Fixed minor issues

--- src/JavascriptReview.jsx
+++ src/JavascriptReview.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { docco } from 'react-syntax-highlighter/dist/esm/styles/prism';
+import SyntaxHighlighter from 'react-syntax-highlighter';
+import { docco } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 
 const scrollTo = (id) => {
   document.querySelector(`#${id}`).scrollIntoView();
