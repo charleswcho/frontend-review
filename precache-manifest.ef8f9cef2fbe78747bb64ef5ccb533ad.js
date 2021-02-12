5cecd6c2bc732ac2cef75e2504e85ae46278c242
Charles Wesley Cho <charleswcho@gmail.com>
2021-02-11 20:49:13 -0800

    Updates

---
+++ precache-manifest.ef8f9cef2fbe78747bb64ef5ccb533ad.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb905e3e8d0843855d505170e27db38b",
+    "url": "/frontend-review/index.html"
+  },
+  {
+    "revision": "7ed57320eb92c8b76dc4",
+    "url": "/frontend-review/static/css/2.b4a350fc.chunk.css"
+  },
+  {
+    "revision": "bca60706e18ebb09d1dc",
+    "url": "/frontend-review/static/css/main.042d783f.chunk.css"
+  },
+  {
+    "revision": "7ed57320eb92c8b76dc4",
+    "url": "/frontend-review/static/js/2.0ca56f0c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/frontend-review/static/js/2.0ca56f0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bca60706e18ebb09d1dc",
+    "url": "/frontend-review/static/js/main.64ccade7.chunk.js"
+  },
+  {
+    "revision": "5bbe61b8e696573cead0",
+    "url": "/frontend-review/static/js/runtime-main.ba47e42c.js"
+  },
+  {
+    "revision": "5cb7edfceb233100075dc9a1e12e8da3",
+    "url": "/frontend-review/static/media/roboto-latin-100.5cb7edfc.woff"
+  },
+  {
+    "revision": "7370c3679472e9560965ff48a4399d0b",
+    "url": "/frontend-review/static/media/roboto-latin-100.7370c367.woff2"
+  },
+  {
+    "revision": "f8b1df51ba843179fa1cc9b53d58127a",
+    "url": "/frontend-review/static/media/roboto-latin-100italic.f8b1df51.woff2"
+  },
+  {
+    "revision": "f9e8e590b4e0f1ff83469bb2a55b8488",
+    "url": "/frontend-review/static/media/roboto-latin-100italic.f9e8e590.woff"
+  },
+  {
+    "revision": "b00849e00f4c2331cddd8ffb44a6720b",
+    "url": "/frontend-review/static/media/roboto-latin-300.b00849e0.woff"
+  },
+  {
+    "revision": "ef7c6637c68f269a882e73bcb57a7f6a",
+    "url": "/frontend-review/static/media/roboto-latin-300.ef7c6637.woff2"
+  },
+  {
+    "revision": "14286f3ba79c6627433572dfa925202e",
+    "url": "/frontend-review/static/media/roboto-latin-300italic.14286f3b.woff2"
+  },
+  {
+    "revision": "4df32891a5f2f98a363314f595482e08",
+    "url": "/frontend-review/static/media/roboto-latin-300italic.4df32891.woff"
+  },
+  {
+    "revision": "479970ffb74f2117317f9d24d9e317fe",
+    "url": "/frontend-review/static/media/roboto-latin-400.479970ff.woff2"
+  },
+  {
+    "revision": "60fa3c0614b8fb2f394fa29944c21540",
+    "url": "/frontend-review/static/media/roboto-latin-400.60fa3c06.woff"
+  },
+  {
+    "revision": "51521a2a8da71e50d871ac6fd2187e87",
+    "url": "/frontend-review/static/media/roboto-latin-400italic.51521a2a.woff2"
+  },
+  {
+    "revision": "fe65b8335ee19dd944289f9ed3178c78",
+    "url": "/frontend-review/static/media/roboto-latin-400italic.fe65b833.woff"
+  },
+  {
+    "revision": "020c97dc8e0463259c2f9df929bb0c69",
+    "url": "/frontend-review/static/media/roboto-latin-500.020c97dc.woff2"
+  },
+  {
+    "revision": "87284894879f5b1c229cb49c8ff6decc",
+    "url": "/frontend-review/static/media/roboto-latin-500.87284894.woff"
+  },
+  {
+    "revision": "288ad9c6e8b43cf02443a1f499bdf67e",
+    "url": "/frontend-review/static/media/roboto-latin-500italic.288ad9c6.woff"
+  },
+  {
+    "revision": "db4a2a231f52e497c0191e8966b0ee58",
+    "url": "/frontend-review/static/media/roboto-latin-500italic.db4a2a23.woff2"
+  },
+  {
+    "revision": "2735a3a69b509faf3577afd25bdf552e",
+    "url": "/frontend-review/static/media/roboto-latin-700.2735a3a6.woff2"
+  },
+  {
+    "revision": "adcde98f1d584de52060ad7b16373da3",
+    "url": "/frontend-review/static/media/roboto-latin-700.adcde98f.woff"
+  },
+  {
+    "revision": "81f57861ed4ac74741f5671e1dff2fd9",
+    "url": "/frontend-review/static/media/roboto-latin-700italic.81f57861.woff"
+  },
+  {
+    "revision": "da0e717829e033a69dec97f1e155ae42",
+    "url": "/frontend-review/static/media/roboto-latin-700italic.da0e7178.woff2"
+  },
+  {
+    "revision": "9b3766ef4a402ad3fdeef7501a456512",
+    "url": "/frontend-review/static/media/roboto-latin-900.9b3766ef.woff2"
+  },
+  {
+    "revision": "bb1e4dc6333675d11ada2e857e7f95d7",
+    "url": "/frontend-review/static/media/roboto-latin-900.bb1e4dc6.woff"
+  },
+  {
+    "revision": "28f9151055c950874d2c6803a39b425b",
+    "url": "/frontend-review/static/media/roboto-latin-900italic.28f91510.woff"
+  },
+  {
+    "revision": "ebf6d1640ccddb99fb49f73c052c55a8",
+    "url": "/frontend-review/static/media/roboto-latin-900italic.ebf6d164.woff2"
+  }
+]);
